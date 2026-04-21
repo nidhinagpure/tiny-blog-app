7360e60dcca14d90991e3d62e1bbeb0de32a946f
nidhinagpure <nagpurenidhi8@gmail.com>
2026-04-22 00:17:11 +0530

    resolved the error in server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { postSignup,postLogin } from "./controllers/user.js";
+import { postSignup,postLogin } from "./../controllers/user.js";
 dotenv.config();
 
 const app = express();
