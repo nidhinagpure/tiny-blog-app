7d25e0f876077f6474f1f0d1a03a6a6c2270f0de
nidhinagpure <nagpurenidhi8@gmail.com>
2026-05-01 16:04:26 +0530

    fix: login api

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router';
-
+import axios from 'axios';
 
 function Login() {
+
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+  const loginUser = async () => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/login`,
+      user
+    );
+    alert(response.data.message); // 👈 show success
+  } catch (error) {
+    alert(error.response?.data?.message || "Login failed");
+  }
+};
+
   return (
     <div>
       <h1 className="text-center text-2xl mt-4 font-bold">Login</h1>
@@ -10,15 +28,19 @@ function Login() {
         <input 
             type="email" 
             placeholder='Enter Your Email'
-            className='border p-2 rounder w-full mb-4 rounded-sm '/>
+            value={user.email}
+            onChange={(e)=> setUser({...user, email: e.target.value})}
+            className='border p-2 rounded w-full mb-4 rounded-sm '/>
         <input 
             type="password" 
-            placeholder='Confirm your password'
-            className='border p-2 rounder w-full mb-4 rounded-sm '
+            placeholder='Enter your password'
+            value={user.password}
+            onChange={(e)=> setUser({...user, password: e.target.value})}
+            className='border p-2 rounded w-full mb-4 rounded-sm '
             />
-        <button className='bg-blue-500 text-white py-2 px-8 rounded hover:bg-blue-600'>Login</button>
+        <button className='bg-blue-500 text-white py-2 px-8 rounded hover:bg-blue-600' onClick={loginUser}>Login</button>
         <p className='mt-2'>Don't have an account?{""}
-            <Link to="/signup" className='text-blue-500 underline' className="text-blue-500 pl-1 underline">Signup</Link>
+            <Link to="/signup" className='text-blue-500 underline'>Signup</Link>
         </p>
       </div>
     </div>
